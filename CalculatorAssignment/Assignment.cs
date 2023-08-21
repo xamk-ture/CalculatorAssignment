6754c1d1bcefb55e911a46bcfa8a4a6c4703b7cd
Tuomas Reijonen <treijonen@live.fi>
2023-08-21 18:55:48 +0300

    Add project files.

---
+++ CalculatorAssignment/Assignment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAssignment
+{
+    public static class Calculator
+    {
+        //decimal offical documentation: https://learn.microsoft.com/en-us/dotnet/api/system.decimal?view=net-7.0
+
+
+        public static decimal Sum(decimal a, decimal b)
+        {
+            //Assigment 1: Return the sum of a and b.
+            throw new NotImplementedException();
+        }
+
+
+        public static decimal Divide(decimal a, decimal b)
+        {
+            //Assigment 2: Return the result of dividing a by b.
+            //Note no need to check for divide by zero, but you can do if you want
+            throw new NotImplementedException();
+        }
+
+        public static decimal Multiply(decimal a, decimal b)
+        {
+            //Assigment 3: Return the result of multiplying a by b.
+            throw new NotImplementedException();
+        }
+
+        public static decimal Subtract(decimal a, decimal b)
+        {
+            //Assigment 4: Return the result of subtracting b from a.
+            throw new NotImplementedException();
+        }
+
+        public static decimal ConvertPoundsToKg(decimal pounds)
+        {
+            //Assigment 5: Return the result of converting pounds to kg. Round to 2 decimals.
+            throw new NotImplementedException();
+        }
+
+        public static decimal ConvertFahreheitToCelcius(decimal fahrenheit)
+        {
+            //Assigment 6: Return the result of converting fahrenheit to celcius.  Round to 2 decimals.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CalculatorAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculatorAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator Assignment");
+
+            // Assignment 1: Return the sum of a and b.
+            Console.WriteLine("Assignment 1: Return the sum of a and b.");
+            Console.WriteLine("Sum(1, 2) = " + Calculator.Sum(1, 2));
+
+            // Assignment 2: Return the result of dividing a by b.
+            Console.WriteLine("Assignment 2: Return the result of dividing a by b.");
+            Console.WriteLine("Divide(1, 2) = " + Calculator.Divide(1, 2));
+
+            // Assignment 3: Return the result of multiplying a by b.
+            Console.WriteLine("Assignment 3: Return the result of multiplying a by b.");
+            Console.WriteLine("Multiply(1, 2) = " + Calculator.Multiply(1, 2));
+
+            // Assignment 4: Return the result of subtracting b from a.
+            Console.WriteLine("Assignment 4: Return the result of subtracting b from a.");
+            Console.WriteLine("Subtract(1, 2) = " + Calculator.Subtract(1, 2));
+
+            // Assignment 5: Return the result of converting pounds to kg.
+            Console.WriteLine("Assignment 5: Return the result of converting pounds to kg.");
+            Console.WriteLine("ConvertPoundsToKg(1) = " + Calculator.ConvertPoundsToKg(1));
+
+            // Assignment 6: Return the result of converting fahrenheit to celcius.
+            Console.WriteLine("Assignment 6: Return the result of converting fahrenheit to celcius.");
+            Console.WriteLine("ConvertFahreheitToCelcius(1) = " + Calculator.ConvertFahreheitToCelcius(1));
+        }
+    }
+}
